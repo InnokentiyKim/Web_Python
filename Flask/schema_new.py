08a77b99d4ee9912389fbb7b775dcaeb9a3c6ffb
inncent <innokentiykim90@gmail.com>
2024-11-28 21:14:07 +0500

    HW_5 Flask app schema

---
+++ Flask/schema_new.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, field_validator
+
+
+class CreateAdv(BaseModel):
+    title: str
+    description: str
+
+
+class UpdateAdv(BaseModel):
+    title: str | None = None
+    description: str | None = None
+
+
+class BaseUser(BaseModel):
+    password: str
+    
+    @field_validator
+    @classmethod
+    def check_password(cls, value: str):
+        if len(value) < 8:
+            raise ValueError("Your password is too short")
+        return value
+    
+
+class CreateUser(BaseUser):
+    name: str
+    password: str
+    
+    
+class UpdateUser(BaseUser):
+    name: str | None = None
+    password: str | None = None
+        
