a967f8c2dcf044ec750549de4e3493807770795e
inncent <innokentiykim90@gmail.com>
2024-12-10 00:11:06 +0500

    HW_6 Asyncio some changes

--- Asyncio/async_requests_new.py
+++ Asyncio/async_requests_new.py
@@ -7,6 +7,20 @@ from models import SwapiPeople, Session, init_orm, close_orm
 
 MAX_COROS = 10
 
+
+async def get_people_count(http_session):
+    url = f"http://swapi.py4e.com/api/people/"
+    http_response = await http_session.get(url)
+    json_data = await http_response.json()
+    return json_data.get("count", 0)
+
+
+async def get_param_from_link(http_session: Session, link: str, param: str) -> str:
+    http_response = await http_session.get(link)
+    json_data = await http_response.json()
+    return json_data.get(param, "")
+
+
 async def get_people(person_id: int, http_session):
     url = f"http://swapi.py4e.com/api/people/{person_id}/"
     http_response = await http_session.get(url)
@@ -24,7 +38,8 @@ async def insert_people(json_list: list[dict] | tuple[dict]):
 async def main():
     await init_orm()
     async with aiohttp.ClientSession() as http_session:
-        for i_list in more_itertools.chunked(range(1, 101), MAX_COROS):
+        amount = await get_people_count(http_session)
+        for i_list in more_itertools.chunked(range(1, amount), MAX_COROS):
             coros = [get_people(i, http_session) for i in i_list]
             result = await asyncio.gather(*coros)
             task = asyncio.create_task(insert_people(result))
@@ -34,6 +49,7 @@ async def main():
     await close_orm()
 
 
-start = datetime.datetime.now()
-asyncio.run(main())
-print(datetime.datetime.now() - start)
+if __name__ == '__main__':
+    start = datetime.datetime.now()
+    asyncio.run(main())
+    print(datetime.datetime.now() - start)
